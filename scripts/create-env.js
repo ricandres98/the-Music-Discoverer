adb4050c68e13fa6e885d488249270052e30e07c
Ricardo Ojeda <ricardo270298@gmail.com>
2022-08-29 11:59:09 -0500

    Multiple keys added to create-env.js

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
 
 
-fs.writeFileSync('./.env', `API_KEY = ${process.env.API_KEY}\nAPI_KEYA = ${process.env.API_KEYA}`)
+fs.writeFileSync('./.env', 
+`API_KEY = ${process.env.API_KEY}
+API_KEYA = ${process.env.API_KEYA}
+API_KEYB = ${process.env.API_KEYB}`)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './sass/mobile.scss';
 
-// import './secrets.js';
 import './modules.js';
 import './navigation.js';
 import './main.js';
